32758f803a452953e0d67e6b161121d7447c030f
nehir <23181616066@gazi.edu.tr>
2026-09-24 09:58:19 +0300

    classes and objects

---
+++ dotnet-learning/Classes & Objects/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotnet_learning.Classes___Objects
+{
+    public class Player
+    {
+        public string username;
+        public int health;
+        public int level;
+
+        public void TakeDamage(int damage)
+        {
+            health = health - damage;
+            Console.WriteLine($"{username} took {damage} damage. remaining health point: {health}");
+
+        }
+
+        public void heal(int healing)
+        {
+            health = health + healing;
+            Console.WriteLine($"{username} healed for {healing}. remaining health point: {health}");
+        }
+    }
+}
--- dotnet-learning/Program.cs
+++ dotnet-learning/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine(Environment.Version);
+﻿using dotnet_learning.Classes___Objects;
+
+Console.WriteLine("***dotnet learning***");
+Console.WriteLine("1. classes and objects\n");
+
+// "new" keyword: "allocate ram memory for a new player"
+Player player1= new Player();
+
+// properties 
+player1.username = "username1";
+player1.health = 100;
+player1.level = 1;
+
+Player player2= new Player();
+
+player2.username = "username2";
+player2.health = 260;
+player2.level = 6;
+
+player1.TakeDamage(30);
+player2.heal(25);
+
